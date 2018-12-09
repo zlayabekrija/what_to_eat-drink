dcc4c1fa0f99d8a7e5ee3dc96593d9449c4442cb
zlayabekrija <zlaja.bekrija@gmail.com>
2018-12-09 18:02:08 +0100

    frame finished,starting with api

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+
+let starter = ['soup','salad'];
+let main = ['meat','fish','vegeterian'];
+let outputStarter = starter[Math.floor(Math.random()*starter.length)];
+let outputMain = main[Math.floor(Math.random()*main.length)];
+window.addEventListener('load', makeRequest);
+
+// function makeRequest() {
+//     httpRequest = new XMLHttpRequest();
+
+//     if (!httpRequest) {
+//         alert(`Can't create request`);
+//         return false;
+//     } else {
+//         httpRequest.onreadystatechange = alertAnswer;
+//         httpRequest.open('GET','https://api.edamam.com/search?q='+outputMain+'&app_id=842f9ed1&app_key=13f969406a60ccfe8ca51e76ddeb2625&from=0&to=3&calories=591-722&health=alcohol-free')
+//         httpRequest.send();
+//     }
+    
+//     function alertAnswer() {
+//         if (httpRequest.readyState === XMLHttpRequest.DONE) {
+//           if (httpRequest.status === 200) {
+//            console.log(httpRequest.responseText);
+//           } else {
+//             alert('There was a problem with the request.');
+//           }
+//         }
+//       }
+
+// }
+
+
+
